d437ee4fcd5860ef24d38fa45ab37d6cf8d3406d
daniel fedo <thecrow34@gmail.com>
2016-01-14 07:53:45 +0200

    yes

---
+++ src/TCPSocketsListener.cpp
@@ -0,0 +1,18 @@
+/*
+ * TCPSocketsListener.cpp
+ *
+ *  Created on: Jan 13, 2016
+ *      Author: user
+ */
+
+#include "TCPSocketsListener.h"
+
+TCPSocketsListener::TCPSocketsListener() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TCPSocketsListener::~TCPSocketsListener() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/TCPSocketsListener.h
@@ -0,0 +1,17 @@
+/*
+ * TCPSocketsListener.h
+ *
+ *  Created on: Jan 13, 2016
+ *      Author: user
+ */
+
+#ifndef TCPSOCKETSLISTENER_H_
+#define TCPSOCKETSLISTENER_H_
+
+class TCPSocketsListener {
+public:
+	TCPSocketsListener();
+	virtual ~TCPSocketsListener();
+};
+
+#endif /* TCPSOCKETSLISTENER_H_ */
